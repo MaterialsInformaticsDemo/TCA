909c591471fee9192226e8395db5b3395508fe6d
Bin Cao <bcao@shu.edu.cn>
2023-07-13 18:52:39 +0800

    update

---
+++ example1/TCA.py
@@ -0,0 +1,56 @@
+# cao bin, HKUST, China
+# free to charge for academic communication
+
+import numpy as np
+import scipy.linalg
+from sklearn.gaussian_process.kernels import  RBF 
+
+class TCA():
+    def __init__(self, dim=30, lamda=1, gamma=1):
+        '''
+        :param dim: data dimension after projection
+        :param lamb: lambda value, Lagrange multiplier
+        :param gamma: length scale for rbf kernel
+        '''
+        self.dim = dim
+        self.lamda = lamda
+        self.kernel = 0.5*RBF(gamma,"fixed")
+
+    def fit(self, Xs, Xt, ):
+        '''
+        :param Xs: ns * m_feature, source domain data 
+        :param Xt: nt * m_feature, target domain data
+        Projecting Xs and Xt to a lower dimension by TCA
+        source/target domain data expressed in a mapping space
+        :return: Xs_new and Xt_new 
+        '''
+        # formular in paper Domain Adaptation via Transfer Component Analysis
+        # Eq.(2) 
+        X = np.vstack((Xs, Xt))
+        K = self.kernel(X)
+        # cal matrix L 
+        ns, nt = len(Xs), len(Xt)
+        if self.dim > (ns + nt):
+            raise DimensionError('The maximum number of dimensions should be smaller than', (ns + nt))
+        else:pass
+        e = np.vstack((1 / ns * np.ones((ns, 1)), -1 / nt * np.ones((nt, 1))))
+        L = e * e.T
+        # cal centering matrix H page 202 the last pargraph at left side
+        n, _ = X.shape
+        H = np.eye(n) - 1 / n * np.ones((n, n))
+        # page 202 the last pargraph at right side
+        matrix = (K @ L @ K + self.lamda * np.eye(n)) @ K @ H @ K.T
+        # cal eigenvalues : w, eigenvectors :V
+        w, V = scipy.linalg.eig(matrix)
+        w, V = w.real, V.real
+        # peak out the first self.dim components
+        ind = np.argsort(w)
+        A = V[:, ind[:self.dim]]
+        # output the mapped data
+        Z = K @ A
+        Xs_new, Xt_new = Z[:ns, :], Z[ns:, :]
+        return Xs_new, Xt_new
+
+
+class DimensionError(Exception):
+    pass
---
+++ example2/example.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import numpy as np
+from sklearn.svm import SVC
+from sklearn.metrics import precision_score
+from TCA import TCA
+
+# source domain training data
+Xs_y = pd.read_csv('Xs_y.csv')
+Xs = np.array(Xs_y.iloc[:,:-1])
+ys = np.array(Xs_y.iloc[:,-1])
+# target domain training data
+Xt_y = pd.read_csv('Xt_y.csv')
+Xt = np.array(Xt_y.iloc[:,:-1])
+yt = np.array(Xt_y.iloc[:,-1])
+# test data
+test = pd.read_csv('Xtest.csv')
+Xtest = np.array(test.iloc[:,:-1])
+ytest = np.array(test.iloc[:,-1])
+
+reg = SVC()
+################################################################
+# case one 
+# training on target domina data (Xt_y.csv) and test on testing dataset (Xtest.csv)
+pre_label = reg.fit(Xt, yt,).predict(Xtest)
+rate = np.sqrt(precision_score(pre_label,ytest))
+print('Without transfer, we have misclassify rate %f' % rate )
+
+# case two
+# training on mapped space (4-d) with source (Xs_y.csv) and target domina data (Xt_y.csv), test on testing dataset (Xtest.csv)
+acc_list = [rate]
+numbers = np.linspace(0.01, 1, 10)
+for j in range(len(numbers)):
+    model = TCA(dim=1, lamda=numbers[j], gamma=0.5)
+    Train_X = np.vstack((Xs,Xt))
+    Xs_new, Xt_new = model.fit(Train_X,Xtest)
+    Train_y = np.hstack((ys,yt))
+    transfer_pre_label = reg.fit(Xs_new, Train_y).predict(Xt_new)
+    transfer_rate = np.sqrt(precision_score(transfer_pre_label,ytest))
+    acc_list.append(transfer_rate)
+    print('lamda = {}'.format(numbers[j]))
+    print('With transfer, we have misclassify rate %f' % transfer_rate ,'\n')
+
+
+# plot the result
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+
+fig, ax = plt.subplots()
+x = range(len(acc_list)-1, 0, -1)
+
+plt.plot(x, acc_list[1:], linestyle='--', color='blue',label='TCA transfer with different lamda')
+#ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+
+plt.xlabel('lamda')
+plt.ylabel('RMSE on test data')
+
+ax.legend()
+plt.savefig('iteration number.png',bbox_inches = 'tight',dpi=600)
+plt.savefig('iteration number.svg',bbox_inches = 'tight',dpi=600)
+plt.show()
+
+
+
